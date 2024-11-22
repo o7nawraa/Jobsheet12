6caa23e2cac155d46a1475a2158e348c3fb7d052
07nawraa <anindyanauraanindya@gmail.com>
2024-11-22 13:47:45 +0700

    ini modify kode no 11 eksperimen 1

--- Cinema07.java
+++ Cinema07.java
@@ -21,8 +21,8 @@ public class Cinema07 {
         }
 
         System.out.println("Audience in the row 3: ");
-        for (int i = 0; i < audience[2].length; i++) {
-            System.out.println(audience[2][i]);
+        for (String i : audience[2]) {
+            System.out.println(i);
         }
     }
 }

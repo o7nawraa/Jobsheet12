8cb115e42f37014629a8ea46b27e200510c9551e
07nawraa <anindyanauraanindya@gmail.com>
2024-11-22 12:43:21 +0700

    ini modify code no 5 eksperimen 1

--- Cinema07.java
+++ Cinema07.java
@@ -8,10 +8,16 @@ public class Cinema07 {
         audience[2][0] = "Eka";
         audience[2][1] = "Farhan";
         audience[3][0] = "Gisel";
+        audience[3][1] = "Hana";
         
         System.out.printf("%s \t %s\n", audience[0][0], audience[0][1]);
         System.out.printf("%s \t %s\n", audience[1][0], audience[1][1]);
         System.out.printf("%s \t %s\n", audience[2][0], audience[2][1]);
         System.out.printf("%s \t %s\n", audience[3][0], audience[3][1]);
+
+        System.out.println(audience.length);
+        for (int i = 0; i < audience.length; i++) {
+            System.out.println("Length of row " + (i+1) + ": " + audience[i].length);
+        }
     }
 }
